803a2ccd6049a57f4c61daf6dd68ccbcb2af4421
InSaNiTyCtEaTuReS <void7535@gmail.com>
2025-12-24 13:40:44 -0600

    Update Main.py to actually encode the script

--- Main.py
+++ Main.py
@@ -1,53 +1,80 @@
-def set(expressions):
-    
-    precedence = {'+': 1, '-': 1, '<<': 1,'>>': 1, '*': 2, '/': 2, '^': 3}
-    output = []
-    output2 = []
-    operator_stack = []
-    expression = ""
-    i = 0
+labels = []
 
 
-    for character in expressions:
-        expression += character
-        if character != " ":
-            expression += " "
+if_counter = 0
+for_counter = 0
+linecount = 0
 
-    tokens = expression.split()
+jump_lut = {
+    ">=":"l",
+    "<=":"g",
+    "==":"ne",
+    "!=":"eq",
+    ">":"le",
+    "<":"ge"
 
-    for token in tokens:
-        i += 1
-        print("token = "+token)
-        
-        if token == "=":
-            output.append(output[0])
-        elif token in variables:
-            c_a("load_16 r"+str(i)+" ["+str(variables.index(token)*4)+"]") #handle variables
-            output.append("r"+str(i))
-        elif token.isnumeric() or (token.startswith('-') and token[1:].isnumeric()):  # Handle numbers (including negative)
-            c_a("mov r"+str(i)+" "+str(token))
-            output.append(token)
-        elif token == '(':
-            operator_stack.append(token)
-        elif token == ')':
-            while operator_stack and operator_stack[-1] != '(':
-                output.append(operator_stack.pop())
-            operator_stack.pop()  # Pop the '('
-        elif token in precedence:
-            while (operator_stack and operator_stack[-1] != '(' and
-                    precedence.get(operator_stack[-1], 0) >= precedence[token]):
-                output.append(operator_stack.pop())
-            operator_stack.append(token)
-        else:
-            raise ValueError(f"Invalid token: {token}")
+}
 
-    c_a("mov r"+str(i)+",r13")
-    while operator_stack:
-        if operator_stack[-1] == '(':
-            raise ValueError("Mismatched parentheses in expression")
-        output.append(operator_stack.pop())
 
-    return " ".join(output2)+' '.join(output) #returns RPN of the inputted expression or expressionlet
+def compile(line):
+    variables = ["zr","for_temp"]
+    spaces = (len(line.lstrip())-len(line))/4
+    line = line.lstrip()
+    print(f"compiling{line}")
+    if (line[0] != "#") and (line != ""):
+        output = ""
+        if line[0:2] == "def":
+            output = ""
+            labels += line[4:-1]
 
-def if_logic(line):
-  print("fuck")
+        elif line[0:2] == "set":   #format: set r1 = variable
+            tokens = line.split()
+            variables += tokens[3]
+            output = "" 
+
+        elif line[0] == "<":
+            tokens = line.split()
+            output = tokens[0] + tokens[1] #allows for comments
+
+        elif line[0:3] == "for ":
+            if_counter += 1
+            tokens = line.split()
+            labels.append([f"labelf_{for_counter}__os",linecount])
+            output = f"sub {tokens[3]},1,{tokens[3]}"
+            output += f"cmpi {tokens[3]},0"
+            
+
+        elif line[0:5] == "forbit":  #forbit means to interpret it as a binary number instead of as a integer
+            if_counter += 1
+            tokens = line.split()
+            labels.append([f"labelf_{for_counter}__os",linecount])
+            output = "sub r1,r1,r1"
+            output += f"muli r{tokens[3]},2,r{tokens[3]}"
+            output += f"andi r{tokens[3]},1,r1"
+
+        elif line[0:3] == "exit":
+            labels.append([f"labelf_{for_counter}_exit__os",linecount])
+            output = f"jae labelf_{for_counter}__os"
+
+        elif line == "break":
+            output = f"jump labelf_{for_counter}_exit__os"
+
+        elif line[0:1] == "if":
+            output = "cmp"
+            tokens = line.split()
+            if not(tokens[3] in variables):
+                output += "i"
+            output += tokens[1] + tokens[3]
+            output += f"j{jump_lut[tokens[2]]}label_{if_counter}_exit__os"
+
+        elif line == "if exit":
+            labels.append([f"label_{if_counter}_exit__os",linecount])
+            output = ""
+
+    else:
+        output = ""
+
+    print(f"line compiled: {line} -> {output}")
+    linecount += 1
+
+compile("set r1 = base")
